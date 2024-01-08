54656091af9b91c6153ef67e0797b93688d40a0a
andreimyc <andreimyc@mail.ru>
2024-01-08 17:30:47 +0300

    add_current_url_in_test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -4,9 +4,10 @@ from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                         # открываем страницу
-    page.go_to_login_page()             # выполняем метод страницы — переходим на страницу логина
+    page = MainPage(browser, link)                      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                                         # открываем страницу
+    page.go_to_login_page()                             # выполняем метод страницы — переходим на страницу логина
+    # print(f"Current URL: {browser.current_url}")      # Выводим текущий URL в консоль
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
