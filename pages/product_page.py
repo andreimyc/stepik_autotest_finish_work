e9a38f9a7c2e95b4889a4ca278a75de62956d539
andreimyc <andreimyc@mail.ru>
2024-01-15 13:50:39 +0300

    update_product_page_file_and_test_product_page_file_and_base_pages_fail_negative_checks

--- pages/product_page.py
+++ pages/product_page.py
@@ -31,6 +31,14 @@ class ProductPage(BasePage):
         basket_total_message = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_MESSAGE).text
         assert product_price == basket_total_message, "The price of the product does not match the success message"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_success_message_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message did not disappear, but should have"
+
     # def should_contain_product_name_in_success_message(self, expected_product_name):
     #     success_message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
     #     assert expected_product_name in success_message, "Product name is not in the success message"
--- test_product_page.py
+++ test_product_page.py
@@ -1,18 +1,42 @@
 import pytest
 from .pages.product_page import ProductPage
 
-@pytest.mark.parametrize('promo_offer', ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"])
-def test_guest_can_add_product_to_basket(browser, promo_offer):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
-    page = ProductPage(browser, link)
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/")
     page.open()
     page.add_to_basket()
-    page.solve_quiz_and_get_code()
-    if promo_offer == "7":
-        pytest.xfail("Test intentionally marked as xfail due to known issue")
-    page.should_be_success_message()
-    page.should_contain_product_name_in_success_message()
-    page.should_contain_product_price_in_basket_total_message()
+    # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/")
+    page.open()
+    # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/")
+    page.open()
+    page.add_to_basket()
+    # Проверяем, что сообщение об успехе исчезает с помощью is_disappeared
+    page.should_success_message_disappear()
+
+# @pytest.mark.parametrize('promo_offer', ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"])
+# def test_guest_can_add_product_to_basket(browser, promo_offer):
+#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_not_be_success_message()
+#     page.add_to_basket()
+#     page.solve_quiz_and_get_code()
+#     if promo_offer == "7":
+#         pytest.xfail("Test intentionally marked as xfail due to known issue")
+#     page.should_be_success_message()
+#     page.should_contain_product_name_in_success_message()
+#     page.should_contain_product_price_in_basket_total_message()
+#     page.should_see_as_disappering_message()
 
 # def test_guest_can_add_product_to_basket(browser):
 #     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
